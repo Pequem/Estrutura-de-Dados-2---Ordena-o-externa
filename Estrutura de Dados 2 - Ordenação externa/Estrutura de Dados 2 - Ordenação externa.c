cd5d702ed7a4532997d59859ae499438fed96204
Pequem <rodrigues.guerra@hotmail.com>
2017-05-16 21:53:33 -0300

    .

--- Estrutura de Dados 2 - Ordenação externa/Estrutura de Dados 2 - Ordenação externa.c
+++ Estrutura de Dados 2 - Ordenação externa/Estrutura de Dados 2 - Ordenação externa.c
@@ -2,6 +2,7 @@
 //
 #include "arq.h"
 #include "bloco.h"
+#include <stdlib.h>
 #define numRegistros 3
 #define true 1
 #define false 0
@@ -13,43 +14,117 @@ typedef FILE* ArqEntradaTipo;
 
 int main(int argc, char **argv)
 {
-	OrdeneExterno(argv[1]);
+	OrdeneExterno("teste.txt");
 	return 0;
 }
 
-ArqEntradaTipo AbrirArquivoDeEntrada(char *arquivoNome) {
-	ArqEntradaTipo arquivo;
-	arquivo = fopen(arquivoNome, "r+");
-	if (arquivo) {
-
+void AbreArqEntrada(ArqEntradaTipo* ArrArqEnt, int Low, int Lim)
+{
+	char name[10];
+	int j = 0;
+	for (int i = Low; i <= Lim; i++)
+	{
+		itoa(i, name, 10);
+		ArrArqEnt[j] = fopen(name , "r+");
+		j++;
 	}
-	return arquivo;
 }
 
-int EnchePaginas(int NBlocos, ArqEntradaTipo arquivo, char *Buffer) {
+int cmpfunc (const void* a, const void* b)
+{
+   return ( *(char*)a - *(char*)b );
+}
+
+ArqEntradaTipo AbrirArquivoDeEntrada(char *arquivoNome) {
+	return fopen(arquivoNome, "r+");
+}
+
+int EnchePaginas(int NBlocos, ArqEntradaTipo arquivo, char *Buffer, int *blocosLidos) {
 	int i;
+	char c;
+	*blocosLidos = 0;
 	for (i = 0; i < numRegistros; i++) {
-		Buffer[i] = fscanf(arquivo, "%c");
-		if (Buffer[i] == EOF) {
+		fread(&c, sizeof(char), 1, arquivo);
+		if (feof(arquivo)) {
+			Buffer[i] = '\0';
 			return true;
 		}
+		Buffer[i] = c;
+		(*blocosLidos)++;
+	}
+	if (feof(arquivo)) {
+		return true;
 	}
 	return false;
 }
 
 ArqEntradaTipo AbreArqSaida(int NBlocos) {
-	char bloco[2];
-	bloco[0] = NBlocos;
-	bloco[1] = '\0';
+	char bloco[10];
+	itoa(NBlocos, bloco, 10);
 	return fopen(bloco, "w");
 }
 
 void DescarregaPaginas(ArqEntradaTipo ArqSaida, char *Buffer) {
 	int i;
+	char c;
 	for (i = 0; i < numRegistros; i++) {
-		if (Buffer[i] == EOF) return;;
-		fprintf(ArqSaida, "%c", &Buffer[i]);
+		if (Buffer[i] == '\0') return;
+		c = Buffer[i];
+		fprintf(ArqSaida, "%c", c);
 	}
+}
+
+char getMenorElemento(char *c, int tam, int *posicao) {
+	int i = 0;
+	//utilizar null;
+	char menor = 127;
+	for (i = 0; i <= tam; i++) {
+		if (menor > c[i]) {
+			menor = c[i];
+			*posicao = i;
+		}
+	}
+	c[*posicao] = 127;
+	return menor;
+}
+
+void Intercale(ArqEntradaTipo *ArrArqEnt, int Low, int Lim, ArqEntradaTipo ArqSaida)
+{
+	const int tam = Lim - Low;
+	int i, j = 0, posicao;
+	char c[OrdemIntercalacao], c1;
+	int blocosFinalizados = 0;
+	for (i = 0; i <= tam; i++) {
+		fread(&c1, sizeof(char), 1, ArrArqEnt[i]);
+		c[j] = c1;
+		j++;
+	}
+	do{
+		c1 = getMenorElemento(c, tam, &posicao);
+		fwrite(&c1, sizeof(char), 1, ArqSaida);
+		fread(&c1, sizeof(char), 1, ArrArqEnt[posicao]);
+		if (feof(ArrArqEnt[posicao])) {
+			blocosFinalizados++;
+		}
+		else {
+			c[posicao] = c1;
+		}
+	}while (blocosFinalizados != OrdemIntercalacao);
+}
+
+int Minimo(int Low, int High)
+{
+	if (Low > High)
+	{
+		return High;
+	}
+	return Low;
+}
+
+void Apague_Arquivo(int i) {
+	char name[10];
+	itoa(i, name, 10);
+	remove(name);
 }
 
 void OrdeneExterno(char *arquivo) {
@@ -58,35 +133,38 @@ void OrdeneExterno(char *arquivo) {
 	ArqEntradaTipo ArqEntrada, ArqSaida;
 	ArqEntradaTipo ArrArqEnt[OrdemIntercalacao];
 	short Fim;
-	int Low, High, Lim;
+	int Low, High, Lim, blocosLidos = 0, i, j;
 	char buffer[numRegistros];
-	NBlocos = 0;
 	ArqEntrada = AbrirArquivoDeEntrada(arquivo);
 	do   /*Formacao inicial dos NBlocos ordenados */
 	{
+		Fim = EnchePaginas(NBlocos, ArqEntrada, buffer, &blocosLidos);
+		if (buffer[0] == '\0') break;
 		NBlocos++;
-		Fim = EnchePaginas(NBlocos, ArqEntrada, buffer);
-		//OrdeneInterno;
-		ArqSaida = AbreArqSaida(NBlocos);
+		qsort(buffer, blocosLidos, sizeof(char), cmpfunc);
+		ArqSaida = AbreArqSaida(NBlocos - 1);
 		DescarregaPaginas(ArqSaida, buffer);
 		fclose(ArqSaida);
 	} while (!Fim);
 	fclose(ArqEntrada);
 	Low = 0;
 	High = NBlocos - 1;
-	//while (Low < High) /* Intercalacao dos NBlocos ordenados */
-	//{
-	//	Lim = Minimo(Low + OrdemIntercalacao - 1, High);
-	//	AbreArqEntrada(ArrArqEnt, Low, Lim);
-	//	High++;
-	//	ArqSaida = AbreArqSaida(High);
-	//	Intercale(ArrArqEnt, Low, Lim, ArqSaida);
-	//	fclose(ArqSaida);
-	//	for (i = Low; i < Lim; i++) {
-	//		fclose(ArrArqEnt[i]);
-	//		Apague_Arquivo(ArrArqEnt[i]);
-	//	}
-	//	Low += OrdemIntercalacao;
-	//}   Mudar o nome do arquivo High para o nome fornecido pelo usuario;
+	while (Low < High) /* Intercalacao dos NBlocos ordenados */
+	{
+		Lim = Minimo(Low + OrdemIntercalacao - 1, High);
+		AbreArqEntrada(ArrArqEnt, Low, Lim);
+		High++;
+		ArqSaida = AbreArqSaida(High);
+		Intercale(ArrArqEnt, Low, Lim, ArqSaida);
+		fclose(ArqSaida);
+		for (i = Low, j = 0; i <= Lim; i++ , j++) {
+			fclose(ArrArqEnt[j]);
+			Apague_Arquivo(i);
+		}
+		Low += OrdemIntercalacao;
+	}   //Mudar o nome do arquivo High para o nome fornecido pelo usuario;
 
 }
+
+
+
