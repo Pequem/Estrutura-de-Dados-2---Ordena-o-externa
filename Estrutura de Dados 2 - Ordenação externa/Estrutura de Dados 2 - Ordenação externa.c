d7b444fbbbc3950f69dc8ce351878b8ccf449b8e
Pequem <rodrigues.guerra@hotmail.com>
2017-06-07 21:57:08 -0300

    .

--- Estrutura de Dados 2 - Ordenação externa/Estrutura de Dados 2 - Ordenação externa.c
+++ Estrutura de Dados 2 - Ordenação externa/Estrutura de Dados 2 - Ordenação externa.c
@@ -10,49 +10,9 @@
 
 int main(int argc, char **argv)
 {
-	int f, e;
-	printf("m/4\n");
-	for (f = 2; f <= 4; f++)
-	{
-		for (e = 20; e <= 22; e++)
-		{
-			ramdomReg("teste1.bin", e);
-			clock_t start = clock();
-			OrdeneExterno("teste1.bin", "final.bin", f, pow(2, e) / 4, sizeof(Reg), cmpFuncReg, NULL, NULL);
-			clock_t end = clock();
-			float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-			printf("f = %d, 2^%d: %f seconds\n", f, e, seconds);
-		}
-	}
-
-	printf("m/16\n");
-	for (f = 2; f <= 4; f++)
-	{
-		for (e = 20; e <= 22; e++)
-		{
-			ramdomReg("teste1.bin", e);
-			clock_t start = clock();
-			OrdeneExterno("teste1.bin", "final.bin", f, pow(2, e) / 16, sizeof(Reg), cmpFuncReg, NULL, NULL);
-			clock_t end = clock();
-			float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-			printf("f = %d, 2^%d: %f seconds\n", f, e, seconds);
-		}
-	}
-
-	printf("m/256\n");
-	for (f = 2; f <= 4; f++)
-	{
-		for (e = 20; e <= 22; e++)
-		{
-			ramdomReg("teste1.bin", e);
-			clock_t start = clock();
-			OrdeneExterno("teste1.bin", "final.bin", f, pow(2, e) / 256, sizeof(Reg), cmpFuncReg, NULL, NULL);
-			clock_t end = clock();
-			float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-			printf("f = %d, 2^%d: %f seconds\n", f, e, seconds);
-		}
-	}
-
-	system("pause");
+	//teste2222
+	ramdomReg("teste1.bin", 16);
+	OrdeneExterno("teste1.bin", "final.bin", 5, 50, sizeof(Reg), cmpFuncReg, NULL, NULL);
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
